fbc1974fb44f70260271d3508c585eec183c42e5
DINGU <web.ljg@gmail.com>
2021-12-28 23:32:10 +0900

    헤더 컴포넌트 스타일링

--- vue-news/src/components/ToolBar.vue
+++ vue-news/src/components/ToolBar.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="header">
     <router-link to="/news">News</router-link> |
     <router-link to="/ask">Ask</router-link> |
     <router-link to="/jobs">Jobs</router-link>
@@ -12,6 +12,8 @@ export default {
 }
 </script>
 
-<style>
-
+<style scoped>
+  .header{padding:8px;color:#fff;background-color:#42b883;}
+  .header .router-link-exact-active{color:#35495e;}
+  .header a{color:#fff;}
 </style>
