495f79213fd356ab8d2c691746654e5958f9f6cf
DINGU <web.ljg@gmail.com>
2021-12-29 22:34:31 +0900

    axios 활용 api 접근

---
+++ vue-news/src/api/index.js
@@ -0,0 +1,25 @@
+import axios from 'axios';
+
+const config = {
+  baseUrl: 'https://api.hnpwa.com/v0/'
+}
+
+function fetchNewsList() {
+  // return axios.get(config.baseUrl + 'news/1.json');
+  // ES6 템플릿 문법
+  return axios.get(`${config.baseUrl}news/1.json`);
+}
+
+function fetchAskList() {
+  return axios.get(`${config.baseUrl}ask/1.json`);
+}
+
+function fetchJobsList() {
+  return axios.get(`${config.baseUrl}jobs/1.json`);
+}
+
+export {
+  fetchNewsList,
+  fetchAskList,
+  fetchJobsList
+}
