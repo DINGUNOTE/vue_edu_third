35279095563ccefd7dd98f7b4320a62688fb7998
DINGU <web.ljg@gmail.com>
2022-01-04 19:18:56 +0900

    NewsView, JobsView, AskView API Vuex 스토어로 이동, 구조 변경

--- vue-news/src/views/AskView.vue
+++ vue-news/src/views/AskView.vue
@@ -1,21 +1,37 @@
 <template>
   <div>
-    <div v-for="item in ask" v-bind:key="item.id"> {{ item.title }} </div>
+    <div v-for="item in fetchedAsk" v-bind:key="item.id"> {{ item.title }} </div>
   </div>
 </template>
 
 <script>
-import { fetchAskList } from '../api/index.js';
+import { mapGetters } from 'vuex';
+
 export default {
-  data() {
-    return {
-      ask: []
-    }
+  computed: {
+
+    // #1
+    // ...mapGetters({
+    //   fetchedAsk: 'fetchedAsk',
+    // })
+
+    // #1-2
+    ...mapGetters([
+      'fetchedAsk'
+    ])
+
+    // #2
+    // ...mapState({
+    //   fetchedAsk: state => state.ask
+    // })
+
+    // #1
+    // ask() {
+    //   return this.$store.state.ask;
+    // }
   },
   created() {
-    fetchAskList()
-      .then(response => this.ask = response.data)
-      .catch(error => console.log(error));
+    this.$store.dispatch('FETCH_ASK');
   }
 }
 </script>
