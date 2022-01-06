985ddaad93a72c647354f6cb82c734e83f7103d9
DINGU <web.ljg@gmail.com>
2022-01-06 14:05:12 +0900

    라우터 트랜지션을 이용해서 페이지 전환 시 애니메이션 추가

--- vue-news/src/App.vue
+++ vue-news/src/App.vue
@@ -1,9 +1,11 @@
 <template>
   <div id="app">
     <ToolBar></ToolBar>
+    <transition name="page">
+      <!-- router에 설정된 url과 맞는 컴포넌트가 표시 됨 -->
+      <router-view></router-view>
+    </transition>
     
-    <!-- router에 설정된 url과 맞는 컴포넌트가 표시 됨 -->
-    <router-view></router-view>
   </div>
 </template>
 
@@ -18,4 +20,14 @@ export default {
 
 <style>
 body{margin:0;padding:0;}
+
+/* Router Transition */
+.page-enter-active, .page-leave-active {
+  transition: opacity .5s;
+}
+.page-enter, .page-leave-to /* .page-leave-active below version 2.1.8 */ {
+  opacity: 0;
+}
+/* // Router Transition */
+
 </style>
