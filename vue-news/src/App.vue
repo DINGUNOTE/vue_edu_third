9b3786274e785b4d6118a364d172a55826c469bc
DINGU <web.ljg@gmail.com>
2022-01-07 22:46:24 +0900

    뉴스리스트 관련 컴포넌트 공통화 리팩토링(1)

--- vue-news/src/App.vue
+++ vue-news/src/App.vue
@@ -20,6 +20,9 @@ export default {
 
 <style>
 body{margin:0;padding:0;}
+a{color:#34495e;text-decoration:none;}
+a:hover{color:#42b883;text-decoration:underline;}
+a.router-link-exact-active{text-decoration:underline;}
 
 /* Router Transition */
 .page-enter-active, .page-leave-active {
