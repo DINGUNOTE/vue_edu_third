360c326e394a687503fd4e30c8a45fd77be49a70
DINGU <web.ljg@gmail.com>
2022-01-11 21:24:05 +0900

    사용자 프로필 컴포넌트 데이터 흐름 처리

---
+++ vue-news/src/components/UserProfile.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="user-container">
+    <div>
+      <i class="fas fa-user"></i>
+    </div>
+    <div class="user-description">
+      <div>{{ info.id }}</div>
+      <div class="time">
+        {{ info.created }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    info: Object,
+  },
+}
+</script>
+
+<style scoped>
+  .user-container {
+    display: flex;
+    padding: 0.5rem;
+    align-items: center;
+  }
+  .fa-user {
+    font-size: 2rem;
+  }
+  .user-description {
+    padding-left: 8px;
+  }
+  .time{
+    font-size: 0.7rem;
+  }
+</style>
--- vue-news/src/views/UserView.vue
+++ vue-news/src/views/UserView.vue
@@ -1,13 +1,15 @@
 <template>
   <div>
-    <p>name : {{ userInfo.id }}</p>
-    <p>karma : {{ userInfo.karma }}</p>
-    <p>created : {{ userInfo.created }}</p>
+    <user-profile :info="userInfo"></user-profile>
   </div>
 </template>
 
 <script>
+import UserProfile from '../components/UserProfile.vue';
 export default {
+  components: {
+    UserProfile,
+  },
   computed: {
     userInfo() {
       return this.$store.state.user;
@@ -15,7 +17,6 @@ export default {
   },
   created() {
     const userName = this.$route.params.id;
-    // axios.get(`https://api.hnpwa.com/v0/user/${userName}.json`);
     this.$store.dispatch('FETCH_USER', userName);
   }
 }
