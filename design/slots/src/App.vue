e66b45e416fc8f72e88d671a5b92c56269200dee
DINGU <web.ljg@gmail.com>
2022-01-21 10:41:17 +0900

    # Slot을 활용한 컴포넌트 디자인 방법

---
+++ design/slots/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <ul>
+      <item>아이템 1</item>
+      <item>아이템 2 <button type="button">Click me</button></item>
+      <item><div>아이템 3</div> <img src="./assets/logo.png" alt="Vue" width="30px" height="30px"></item>
+      <item><div style="color: blue; font-size: 20px;">아이템 4</div></item>
+    </ul>
+  </div>
+</template>
+
+<script>
+import Item from './Item.vue';
+
+export default {
+  components: {
+    Item,
+  }
+}
+</script>
